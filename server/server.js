e7ca23c35867a83ea3c68939b2312e855be8722b
gbfc2030-ops <gbfc2030@gmail.com>
2026-01-31 07:01:27 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,4 +39,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); [11, 12]
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
